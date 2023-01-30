1a757685207c45fbe1ad5fb523a5cd1371d4867b
Nikita Balashov <nikita.89534588306@gmail.com>
2023-01-30 14:14:56 +0500

    create header html

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' href='css/style.css'	>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<title>My blog</title>
+</head>
+<body>
+	
+	<header>
+		<div class="container">
+			<div class='row'>
+
+				<div class='header-logo col-4'>
+					<h1 style='margin: 0; padding: 0;'><a href="/"> My blog</a></h1>				
+				</div>
+			
+				<ul class='header-navbar col-8'>
+					<li class='navbar-item'><a href='#'>Главная</a></li>
+					<li class='navbar-item'><a href='#'>О нас</a></li>
+					<li class='navbar-item'><a href='#'>Услуги</a></li>
+					<li class='navbar-item navbar-user-account'>
+						<a href='#'><i class='fa fa-user' style="padding-right:8px; display:inline-block;"></i>Кабинет</a>
+						<ul class='user-account'>
+							<li class='user-account-item'><a href='#'>Админ панель</a></li>
+							<li class='user-account-item'><a href='#'>Выход</a></li>
+						</ul>
+					</li>	
+				<ul>
+			</div>
+		</div>
+	</header>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
